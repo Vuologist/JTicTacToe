541fc35f4046a63215657ea7803481f99ac6c29a
Anthony Vu <anthony.vu2142@gmail.com>
2018-01-20 14:40:54 -0800

    working

--- src/TicTacToeGUI.java
+++ src/TicTacToeGUI.java
@@ -2,39 +2,62 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
+/**
+ *  Name:       Vu, Anthony
+ *  Homework:   1
+ *  Due:        1/22/18
+ *  Course:     cs24501-w18
+ *
+ *  Description:
+ *      This program is responsible for creating a Tic Tac Toe GUI
+ *      playing board and will be rendered through java swing. The
+ *      user will be able to interact with the board. This a 2
+ *      player game with no ai opponent implemented.
+ */
 public class TicTacToeGUI implements ActionListener {
-    public static void main (String args[]){
+    JButton button[] = new JButton[9];
+    int counter = 0;
 
-        JFrame jfrm = new JFrame("Tic Tac Toe");
-
-        jfrm.getContentPane().setLayout(new GridLayout());
-
-        jfrm.setSize (960, 540);
-
-        jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        /* board representation
+    /* board representation
         1   2   3
         4   5   6
         7   8   9
         */
-        JButton button1 = new JButton("button1");
-        JButton button2 = new JButton("button2");
-        JButton button3 = new JButton("button3");
-        JButton button4 = new JButton("button4");
-        JButton button5 = new JButton("button5");
-        JButton button6 = new JButton("button6");
-        JButton button7 = new JButton("button7");
-        JButton button8 = new JButton("button8");
-        JButton button9 = new JButton("button9");
+    public TicTacToeGUI(){
 
+        JFrame jfrm = new JFrame("Tic Tac Toe");
+        jfrm.setLayout(new GridLayout(3,3));
+        jfrm.setSize (500, 500);
+        jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jfrm.setVisible(true);
 
+        for(int i = 0; i < button.length; i++) {
+            button[i] = new JButton("blank");
+            button[i].addActionListener(this);
+            jfrm.add(button[i]);
+        }
     }
 
-
     @Override
-    public void actionPerformed(ActionEvent e) {
+    public void actionPerformed(ActionEvent ae) {
+        if(!ae.getActionCommand().equals("blank"))
+            return;
 
+        if(counter%2 == 0){
+            ((JButton)ae.getSource()).setText("X");
+            counter++;
+        } else {
+            ((JButton)ae.getSource()).setText("O");
+            counter++;
+        }
+    }
+
+    public static void main (String args[]){
+        System.out.println("A. Vu Tic Tac Toe");
+        SwingUtilities.invokeLater(new Runnable(){
+            public void run(){
+                new TicTacToeGUI();
+            }
+        });
     }
 }
